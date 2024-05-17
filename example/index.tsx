c37a90dea0008ddc92e3ff130c55a3ce19b997e1
Matteo Cavallo <m.cavallo1011@gmail.com>
2024-05-17 16:54:13 +0200

    chore: added ubuntu font as dependency

--- example/index.tsx
+++ example/index.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 
+import "@fontsource/ubuntu/300.css";
+import "@fontsource/ubuntu/400.css";
+import "@fontsource/ubuntu/500.css";
+import "@fontsource/ubuntu/700.css";
+
 import "../src/sass/main.scss";
 
 import App from "./App";
